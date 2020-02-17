fce12626516a0284c2ccdbb135dcacae8ad44a23
ssklykov <ssklykov@yandex.ru>
2020-02-17 17:31:35 +0100

    Path manipulation instruments

---
+++ SystemUtil/PathsDemo2.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Continuing to test (make) some features related to os and sys modules
+@author: ssklykov
+"""
+# %% Import section
+import os
+
+# %% Portability utilities
+dirs_separator = os.sep # returning the system-specific directories separator
+print(dirs_separator," - system separator")
+parent_dir = os.pardir
+os.chdir(parent_dir); print(os.getcwd()," - parent directory")
+separated_path = os.getcwd().split(dirs_separator) # Separation using specific directories separator
+restored_path = os.path.join(os.getcwd(),"SystemUtil")
+print(restored_path,"- a restored path after going to a parent directory")
+print(os.path.exists(restored_path),' - does restored path exists?')
+print(os.path.isdir(restored_path)," - a folder is on the restored path")
+os.chdir(restored_path) # change the working directory back
+absol_curr_path = os.path.abspath(os.curdir) # Other way to get an absolute path to cwd
+absol_parent_path = os.path.abspath(os.pardir) # Other way to get an absolute path to parent directory
+print(os.listdir(absol_parent_path), "- list of directories inside this repo")
