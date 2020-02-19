d466936b0cd6ad6a60f193302c21191661117b92
ssklykov <ssklykov@yandex.ru>
2020-02-19 16:24:09 +0100

    Small file 2

---
+++ SystemUtil/ShellArgs.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Testing command-line arguments
+@author: ssklykov
+"""
+import sys
+print("For fully explore of this feature - use the IPython console 'runfile' function")
+print(sys.argv,"- passed to this script arguments from a shell")
