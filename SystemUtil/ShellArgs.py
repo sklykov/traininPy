2b1681a62de509c3f5f45a64bac90e4896e2211e
ssklykov <ssklykov@yandex.ru>
2020-02-19 17:18:51 +0100

    Get shell variables

--- SystemUtil/ShellArgs.py
+++ SystemUtil/ShellArgs.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
 """
-Testing command-line arguments
+Testing command-line arguments + Shell Variables
 @author: ssklykov
 """
 import sys
-print("For fully explore of this feature - use the IPython console 'runfile' function")
+print("For fully exploring of this feature - use the IPython console 'runfile' function")
 print(sys.argv,"- passed to this script arguments from a shell")
+
+# %% Fetching Shell variables
+import os
+keys = list(os.environ.keys())
+keys = keys[0:5]
+for i in keys:
+    print(i,"->",os.environ[i])
