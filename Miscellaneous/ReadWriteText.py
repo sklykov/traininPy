eedc48b5d881c4d7b9d398934000a111d3a6c79b
ssklykov <ssklykov@yandex.ru>
2020-03-02 17:44:40 +0100

    Simple reading / writing txt file

---
+++ Miscellaneous/ReadWriteText.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+Training create / read txt files - again
+@author: ssklykov
+"""
+lines = ['raz','dva','tri','this is the end']
+endline = '\n'
+# %% Demo writing
+with open("demo.txt",'w') as wfile:
+    for line in lines:
+        wfile.write(line + endline)
+# %% Demo reading - in old manner
+with open("demo.txt",'r') as rfile:
+    lines = rfile.readlines()
+    for line in lines:
+        print(line,end = '')
+
+# %% Demo appending
+with open("demo.txt",'a') as afile:
+    afile.write('Now this is the end')
+
+# %% Demo reading - less memory consuming
+with open("demo.txt",'r') as rfile:
+    readed_lines = []
+    for line in rfile:
+        readed_lines.append(line)
+
+# %% Cleaning up
+import os
+os.remove("demo.txt")
