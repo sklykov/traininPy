230a8ee429c6e416a8e6e1a7e16f81ff823bab8b
ssklykov <ssklykov@yandex.ru>
2020-04-16 11:48:05 +0200

    Small changes 4

--- SystemUtil/PathsDemo1.py
+++ SystemUtil/PathsDemo1.py
@@ -1,38 +1,59 @@
 # -*- coding: utf-8 -*-
 """
-Sys and os path manipulations
+Sys and os path manipulations - refactored
 
 @author: ssklykov
 """
-import os, sys
+import os
+import sys
 
 # %% Classical way of getting current working directory
 # print(sys.path) # Printing all evolved in searching modules paths
 # print(os.getcwd()); init_path = str(os.getcwd()) # Classical way for getting
 
-# %% The problem - including other folder to the sys.path. Solution inspired by proffesionals from the StackOverflow
 
+# %% The method returning list of folders in the repository
+def projectImportTree() -> list:
+    """ Get a list with all folders in the repository (.git folder as an anchor)"""
+    anchorFolder = ".git"  # Assuming that this folder always in the holding Git repository
+    listOfDirs = os.listdir(os.getcwd())
+    print(listOfDirs)
+    i = 10  # Maximum number of iterations (in a folder tree)
+    while not(anchorFolder in listOfDirs):
+        os.chdir("..")
+        listOfDirs = os.listdir(os.getcwd())
+        # print(listOfDirs)  # Debugging
+        i += 1
+    return os.listdir(os.getcwd())
+
+
+# %% Simple demo
+allDirs = projectImportTree()
+# %% The problem - including other folder to the sys.path. Solution inspired by professionals from the StackOverflow
 # Getting absolute path to the other folder
-print(os.path.abspath(os.curdir)) # Printing path to a current dir
-os.chdir("..") # Unix-like command to go in a directory tree up
-head_tree = os.path.abspath(os.curdir) # get up on one level in a directory tree
-listOfDirs = os.listdir(head_tree) # To avoid many printing statements - refer to the inspection of variables window
+print(os.path.abspath(os.curdir))  # Printing path to a current dir
+os.chdir("..")  # Unix-like command to go in a directory tree up
+head_tree = os.path.abspath(os.curdir)  # get up on one level in a directory tree
+listOfDirs = os.listdir(head_tree)  # To avoid many printing statements - refer to the inspection of variables window
 go_to_folder = os.listdir(head_tree)[2]
 os.chdir(go_to_folder)
 other_dir = os.path.abspath(os.curdir)
 
 # Adding this directory in the search path
-sysPath = sys.path # Refer to the inspection window for details
+sysPath = sys.path  # Refer to the inspection window for details
 
 # Checking the added folder and deleting extra ones
 if (sys.path.count(other_dir) == 0):
     sys.path.append(other_dir)
 elif (sys.path.count(other_dir) > 1):
-    for i in (2,sys.path.count(other_dir)):
+    for i in (2, sys.path.count(other_dir)):
         sys.path.remove(other_dir)
 
-sysPath = sys.path # Refer to the inspection window for details
+sysPath = sys.path  # Refer to the inspection window for details
 
 # %% Cause additional folder from the entire repository has been added, below is importing some module from it
 from SimpleDatabase_Class import Quad2DImage
-qi = Quad2DImage(2); intensities = qi.I
+qi = Quad2DImage(2)
+intensities = qi.I
+
+
--- SystemUtil/PathsDemo2.py
+++ SystemUtil/PathsDemo2.py
@@ -1,31 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-Continuing to test (make) some features related to os and sys modules
+Continuing to test (make) some features related to os and sys modules (refactored)
 @author: ssklykov
 """
 # %% Import section
 import os
 
 # %% Portability utilities
-dirs_separator = os.sep # returning the system-specific directories separator
-print(dirs_separator," - system separator")
+dirs_separator = os.sep  # Returning the system-specific directories separator
+print(dirs_separator, " - system separator")
 parent_dir = os.pardir
-os.chdir(parent_dir); print(os.getcwd()," - parent directory")
-separated_path = os.getcwd().split(dirs_separator) # Separation using specific directories separator
-restored_path = os.path.join(os.getcwd(),"SystemUtil")
-print(restored_path,"- a restored path after going to a parent directory")
-print(os.path.exists(restored_path),' - does restored path exists?')
-print(os.path.isdir(restored_path)," - a folder is on the restored path")
-os.chdir(restored_path) # change the working directory back
-absol_curr_path = os.path.abspath(os.curdir) # Other way to get an absolute path to cwd
-absol_parent_path = os.path.abspath(os.pardir) # Other way to get an absolute path to parent directory
+os.chdir(parent_dir)
+print(os.getcwd(), " - parent directory")
+separated_path = os.getcwd().split(dirs_separator)  # Separation using specific directories separator
+restored_path = os.path.join(os.getcwd(), "SystemUtil")
+print(restored_path, "- a restored path after going to a parent directory")
+print(os.path.exists(restored_path), ' - does restored path exists?')
+print(os.path.isdir(restored_path), " - a folder is on the restored path")
+os.chdir(restored_path)  # change the working directory back
+absol_curr_path = os.path.abspath(os.curdir)  # Other way to get an absolute path to cwd
+absol_parent_path = os.path.abspath(os.pardir)  # Other way to get an absolute path to parent directory
 print(os.listdir(absol_parent_path), "- list of directories inside this repo")
 
 # %% A bit more features
 listOfFilesInCWD = sorted(os.listdir("."))
-listOfFilesInPWD = sorted(os.listdir("..")) # sorted list of files in a parent directory
+listOfFilesInPWD = sorted(os.listdir(".."))  # sorted list of files in a parent directory
 
 # %% Walking in the directory tree
-for (current_directory,sub_dirs,dir_files) in os.walk('.'):
+for (current_directory, sub_dirs, dir_files) in os.walk('.'):
     for fname in dir_files:
-        print(os.path.join(current_directory,fname))
+        print(os.path.join(current_directory, fname))
