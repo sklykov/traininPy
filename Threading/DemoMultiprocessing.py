8fac2114d5b37d532014b03e1ec3bf8d0a2a32c0
ssklykov <ssklykov@yandex.ru>
2020-04-21 23:59:51 +0200

    Multiprocessing demo

---
+++ Threading/DemoMultiprocessing.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""Simple demo of multiprocessing module using.
+The main idea, seems, that all spawned childrens evoked in a order, but the main thread stops before
+all spawned children stopped.
+@author: ssklykov
+"""
+# %% Import section
+import os
+from multiprocessing import Process, Lock
+import time
+
+
+# %% Definition of methods
+def simpleMethod(label, lock):
+    """For conveying with good style of Python code.
+
+    Simple printing function - the competitor for stdout.
+    Above also two strings as example of good Pydoc style.
+    """
+    with lock:
+        time.sleep(0.4)  # A delay - resembling some work
+        print("The process with data: ", label, os.getpid(), "running")
+
+
+# %% Main process
+lock = Lock()  # The lock for the processes
+p = Process(target=simpleMethod, args=("spawned child", lock))
+p.start()
+p.join()
+
+for i in range(5):
+    Process(target=simpleMethod, args=(("process %s" % i), lock)).start()
+
+with lock:
+    print("Main thread finished")
