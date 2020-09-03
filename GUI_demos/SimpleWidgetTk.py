12bf1b090827f912b94cfcd74f7dd3d3478a94c7
ssklykov <ssklykov@yandex.ru>
2020-09-03 23:21:39 +0200

    Start of GUI demos

---
+++ GUI_demos/SimpleWidgetTk.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Simple experiments on GUI buildings guided by the book Programming Python
+@author: ssklykov
+"""
+from tkinter import Label, YES, BOTH, mainloop
+Label(text="Demo text in this frame").pack(expand=YES, fill=BOTH)
+mainloop()
