127528c29d43583300cdbcfb042e6baaa87f28a8
ssklykov <ssklykov@yandex.ru>
2022-03-23 14:07:53 +0100

    Investigating an issue with stdout and IPython console printing

--- Multiprocesses/simpleProcess.py
+++ Multiprocesses/simpleProcess.py
@@ -7,6 +7,7 @@ Simple multiprocessing Process implementation.
 # %% Imports
 from multiprocessing import Process, Queue
 import time
+import sys
 
 
 # %% Class implementation
@@ -34,6 +35,11 @@ class SimpleProcess(Process):
         """
         for i in range(self.n_steps):
             self.processed_array.append(i + self.some_id)
+            # sys.stdout.write(("appended value:" + str(i + self.some_id)))
+            print(str(i + self.some_id), file=sys.stdout, flush=True)
+            # sys.stdout.write(str(i + self.some_id) + "\n")
+            # self.data_queue.put_nowait(sys.stdout.write(str(i + self.some_id) + "\n"))
+            # sys.stdout.flush()
             time.sleep(0.15)  # Simulation of some work
         self.data_queue.put_nowait(self.processed_array)
 
@@ -41,12 +47,30 @@ class SimpleProcess(Process):
 # %% Tests
 if __name__ == "__main__":
     data_queue1 = Queue(); data_queue2 = Queue()
-    process1 = SimpleProcess(1, data_queue1, n_steps=5); process2 = SimpleProcess(2, data_queue2, n_steps=6)
+    process1 = SimpleProcess(1, data_queue1, n_steps=2); process2 = SimpleProcess(2, data_queue2, n_steps=3)
     process1.start(); process2.start(); process1.join(); process2.join()
-    # !!! Returns empty arrays, as they initialized - below
+    # For getting back the job results, use Queue
     print("Attempt to directly acces to Process variables:", process1.processed_array, process2.processed_array)
     if not(data_queue1.empty()) and (data_queue1.qsize() > 0):
         print("Processed array from 1st process:", data_queue1.get_nowait())
     if not(data_queue2.empty()) and (data_queue2.qsize() > 0):
         print("Processed array from 1st process:", data_queue2.get_nowait())
-    # !!! So for returning processed values, use Queue or other communication tools!
+
+    # Tests concerning access to the standard output reimplmented by the Spyder IDE !!!
+    b = sys.stdout.__dict__
+    # for i in b.keys():
+    #     print(i, ":", b[i])
+    buffer = sys.stdout._buffer
+    print("**************Buffer methods**************")
+    print(dir(buffer))
+    print(buffer.readlines())
+    print("**************Buffer content**************")
+    print(buffer.getvalue())
+
+    # ??? The code above will work if ONLY it's run by the Spyder IDE, which substitutes the sys.stdout module
+    # with some functionality of _io module instead of io. The buffer NOT retains its content by the end of the call,
+    # so the result of calling buffer.getvalue() is different between calls.
+    # The aim of tests here was to write to sys.stdout and print it to the console evoked by the Spyder IDE, but
+    # it isn't feasible, seems.
+    # If this code is run in the dediated independent console, then the stdout works as expected, reporting all the messages
+    # called by print statement in the child processes (see print statement above)
